fa36b549b47c6c6d9e0724e7ef48b5660ce58323
Serhii <sergei181989@gmail.com>
2021-12-21 23:15:59 +0200

    :tada: Creating project_musik & configuring architecture.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
+  experimental: { esmExternals: true },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/scss')],
+  },
 }
