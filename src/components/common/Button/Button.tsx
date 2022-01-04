5deada4f579279eb7aabf717cd9cc6ac88b8eedf
Serhii <sergei181989@gmail.com>
2022-01-04 18:59:35 +0200

    :bug: Fixed animation components & fixed Button component.

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -7,13 +7,20 @@ export interface ButtonProps {
   children?: ReactNode;
   className?: string;
   leftIcon?: boolean;
+  rightIcon?: boolean;
 }
 
-export const Button: FC<ButtonProps> = ({ children, className, leftIcon = false }) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  leftIcon = false,
+  rightIcon = false,
+}) => {
   return (
     <button className={classNames(styles.button, className)}>
       {leftIcon && <Icon name="add" className={styles.add} />}
       <span className={styles.button__title}>{children}</span>
+      {rightIcon && <Icon name="arrow" className={styles.arrow} />}
     </button>
   );
 };
--- src/components/home/HomeAboutUs/HomeAboutUs.tsx
+++ src/components/home/HomeAboutUs/HomeAboutUs.tsx
@@ -1,19 +1,31 @@
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { Button, Container } from '../../common';
 import aboutImage from '../../../../public/about.jpg';
 import styles from './HomeAboutUs.module.scss';
+import gsap from 'gsap';
 
 export const HomeAboutUs: FC = () => {
+  const image = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let tl = gsap.timeline();
+
+    tl.from(image.current, { opacity: 0, x: -100 }).from(content.current, {
+      opacity: 0,
+      x: 100,
+    });
+  });
+
   return (
     <section className={styles.about}>
       <Container>
         <div className={styles.about__wrapper}>
-          <div className={styles.wrapper__images}>
+          <div className={styles.wrapper__images} ref={image}>
             <Image src={aboutImage} alt="About Image" className={styles.about__wrapper_image} />
           </div>
-
-          <div className={styles.about__wrapper_content}>
+          <div className={styles.about__wrapper_content} ref={content}>
             <h2 className={styles.about__wrapper_content__title}>about musik</h2>
             <p className={styles.about__wrapper_content__description}>
               Fusce justo mi, vehicula id arcu et, dapibus tristique lectus. Vivamus a elit sodales,
@@ -21,7 +33,9 @@ export const HomeAboutUs: FC = () => {
               mauris. Integer ut lectus non neque suscipit luctus. Mauris et erat id ipsum
               condimentum cursus. Sed tempus enim non massa mattis iaculis. In quis massa risus
             </p>
-            <Button className={styles.about__wrapper_content__btn}>learn more</Button>
+            <Button rightIcon className={styles.about__wrapper_content__btn}>
+              learn more
+            </Button>
           </div>
         </div>
       </Container>
